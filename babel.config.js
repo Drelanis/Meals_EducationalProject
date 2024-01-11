a9ca27acc87f1f4badab74283a341cfdadb31eab
Denys <denisbadakaty51@gmail.com>
2024-01-11 12:42:41 +0200

    init

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "@babel/plugin-proposal-export-namespace-from",
+      [
+        "module-resolver",
+        {
+          alias: {
+            constants: "./src/constants",
+            components: "./src/components",
+            assets: "./assets",
+          },
+        },
+      ],
+    ],
+  };
+};
