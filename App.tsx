ca42916f41fc2f0e1d4b465a31b19595536a1329
Denys <denisbadakaty51@gmail.com>
2024-01-11 17:39:32 +0200

    minor fix

--- App.tsx
+++ App.tsx
@@ -1,10 +1,7 @@
 import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { FavoritesContextProvider } from "@store/context";
 import { StackNavigator } from "@components/StackNavigator";
-
-const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
