505a94861af75f5aa8399b92bf96c74b97ebab43
Denys <denisbadakaty51@gmail.com>
2024-01-11 15:36:34 +0200

    add MealDetail

---
+++ src/components/MealDetail/Subtitle.tsx
@@ -0,0 +1,28 @@
+import { PropsWithChildren } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export const Subtitle = (props: PropsWithChildren) => {
+  const { children } = props;
+
+  return (
+    <View style={styles.subtitleContainer}>
+      <Text style={styles.subtitle}>{children}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  subtitle: {
+    color: "#e2b497",
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  subtitleContainer: {
+    padding: 6,
+    marginHorizontal: 12,
+    marginVertical: 4,
+    borderBottomColor: "#e2b497",
+    borderBottomWidth: 2,
+  },
+});
