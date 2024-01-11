0570d29fde0ca957a4b1b67d03cb0c5e82086c90
Denys <denisbadakaty51@gmail.com>
2024-01-11 13:57:52 +0200

    add store

---
+++ src/store/context/favorites-meals.context.tsx
@@ -0,0 +1,41 @@
+import { PropsWithChildren, createContext, useState } from "react";
+
+type FavoritesContextProps = {
+  ids: string[];
+  addFavorite: (id: string) => void;
+  removeFavorite: (id: string) => void;
+};
+
+const FavoritesContext = createContext<FavoritesContextProps>({
+  ids: [],
+  addFavorite: (id: string) => {},
+  removeFavorite: (id: string) => {},
+});
+
+export const FavoritesContextProvider = (props: PropsWithChildren) => {
+  const { children } = props;
+
+  const [favoriteMealIds, setFavoriteMealIds] = useState<string[]>([]);
+
+  const addFavorite = (id: string) => {
+    setFavoriteMealIds((currentFavIds) => [...currentFavIds, id]);
+  };
+
+  const removeFavorite = (id: string) => {
+    setFavoriteMealIds((currentFavIds) =>
+      currentFavIds.filter((mealId) => mealId !== id)
+    );
+  };
+
+  const value = {
+    ids: favoriteMealIds,
+    addFavorite,
+    removeFavorite,
+  };
+
+  return (
+    <FavoritesContext.Provider value={value}>
+      {children}
+    </FavoritesContext.Provider>
+  );
+};
