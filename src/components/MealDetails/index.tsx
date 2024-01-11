17ab5e344c1db0d22e44f18b76776e080ccbae2a
Denys <denisbadakaty51@gmail.com>
2024-01-11 14:13:20 +0200

    add MealDetails

---
+++ src/components/MealDetails/index.tsx
@@ -0,0 +1,38 @@
+import { View, Text, StyleSheet, TextStyle, ViewStyle } from "react-native";
+
+type Props = {
+  duration: string;
+  complexity: string;
+  affordability: string;
+  style?: ViewStyle;
+  textStyle?: TextStyle;
+};
+
+export const MealDetails = (props: Props) => {
+  const { duration, complexity, affordability, style, textStyle } = props;
+
+  return (
+    <View style={[styles.details, style]}>
+      <Text style={[styles.detailItem, textStyle]}>{duration}m</Text>
+      <Text style={[styles.detailItem, textStyle]}>
+        {complexity.toUpperCase()}
+      </Text>
+      <Text style={[styles.detailItem, textStyle]}>
+        {affordability.toUpperCase()}
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  details: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 8,
+  },
+  detailItem: {
+    marginHorizontal: 4,
+    fontSize: 12,
+  },
+});
